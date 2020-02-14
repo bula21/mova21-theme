95dff55d6e42862d1cdba420684ac20d88b8879f
Lemming <clement.devaulchier@bula21.ch>
2020-02-14 18:29:41 +0100

    groupes done

--- groupes.php
+++ groupes.php
@@ -5,17 +5,17 @@
 <?php if ( have_posts() ) : while ( have_posts() ) :
 	the_post(); ?>
 <div class="container mod-groupes">
-    <div class="col-12">
-        <h1 class="post-title"><?php the_title(); ?></h1>
-        <div class="wysiwyg">
-            <?php the_content(); ?>
+    <div class="header">
+        <div class="col-12">
+            <h1 class="post-title"><?php the_title(); ?></h1>
+            <div class="wysiwyg">
+                <?php the_content(); ?>
+            </div>
         </div>
+        <p class="text-intro"><?php the_field('intro_text'); ?></p>
     </div>
     <div class="timeLine">
-       <div class="intro">
-           <p class="text-intro"><?php the_field('intro_text'); ?></p>
-       </div>
-        
+
         <?php        
                 if( have_rows('group_timeline') ):
                     while ( have_rows('group_timeline') ) : the_row();
@@ -23,14 +23,17 @@
                         if( have_rows('timeline_event') ):
                         while ( have_rows('timeline_event') ) : the_row(); ?>
 
-                            <div class="timeLine">
-                                <div class="event">
-                                    <p class="date"><?php the_sub_field('event_date'); ?></p>
-                                    <div class="keyPoint"></div>
-                                    <p class="eventName"><?php the_sub_field('event_name'); ?></p>
-                                    <button><?php the_sub_field('event_button'); ?></button>
-                                </div>
-                            </div>
+        <div class="timeLine-event">
+            <div class="event">
+                <div class="keyPoint">
+                    <p class="date"><?php the_sub_field('event_date'); ?></p>
+                </div>
+                <p class="eventName"><?php the_sub_field('event_name'); ?></p>
+                <?php if( get_sub_field('event_button') ): ?>
+                <button><?php the_sub_field('event_button'); ?></button>
+                <?php endif; ?>
+            </div>
+        </div>
 
         <?php endwhile; else : endif;
                     endwhile; else : endif;
@@ -41,17 +44,18 @@
 
             if( have_rows('tm-event-info') ):
             while ( have_rows('tm-event-info') ) : the_row(); ?>
-        
-                <div class="info">
-                   <div class="infoContainer">
-                        <p class="number"><?php the_sub_field('event_number'); ?></p>
-                        <h2><?php the_sub_field('event_name'); ?></h2>
-                        <p class="text"><?php the_sub_field('event_description'); ?></p>
-                        <button><?php the_sub_field('event_button'); ?></button>
-                    </div>
-                </div>
-        
-            <?php endwhile; else : endif;
+
+        <div class="info">
+            <p class="date"><?php the_sub_field('event_date'); ?></p>
+            <h2><?php the_sub_field('event_name'); ?></h2>
+            <div class="callToAction">
+                <p class="text"><?php the_sub_field('event_text'); ?></p>
+                <button><?php the_sub_field('event_button'); ?></button>
+            </div>
+            <div class="cover"></div>
+        </div>
+
+        <?php endwhile; else : endif;
         ?>
     </div>
 
