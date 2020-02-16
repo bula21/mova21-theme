81c4df0128e9741e6f66bc60e99293809bf82ddb
Florian Bentele <florian.bentele@alea-iacta.ch>
2020-02-16 15:31:57 +0100

    added animation on hover only and hidden on mobile

--- js/mod-animation.js
+++ js/mod-animation.js
@@ -4,12 +4,21 @@ jQuery(document).ready(function ($) {
 
     $(module).each(function (id, ctx) {
         var animationName = $(ctx).data('animation');
-        lottie.loadAnimation({
+        var hover = $(ctx).data('hover') ? true : false; // we need real bools
+        var autoplay = $(ctx).data('autoplay') ? true : false;
+        var animation = lottie.loadAnimation({
             container: $(ctx).get(0),
             renderer: 'svg',
             loop: true,
-            autoplay: true,
+            autoplay: autoplay,
             path: path + animationName + '/data.json'
         });
+        if (hover) {
+            $(ctx).hover(function () {
+                animation.play();
+            }, function () {
+                animation.pause();
+            })
+        }
     });
 });
