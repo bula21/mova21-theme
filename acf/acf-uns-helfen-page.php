7300ad40bc270bcabddc7b0dc0eb8772f27968aa
Lemming <clement.devaulchier@bula21.ch>
2020-02-16 12:23:11 +0100

    Collapsable item on unsHelfen page

--- acf/acf-uns-helfen-page.php
+++ acf/acf-uns-helfen-page.php
@@ -1,87 +1,90 @@
 <?php
-if ( function_exists( 'acf_add_local_field_group' ) ):
 
-	acf_add_local_field_group( array(
-		'key'                   => 'group_5e443fb80dbaa',
-		'title'                 => 'Uns Helfen Page',
-		'fields'                => array(
-			array(
-				'key'               => 'field_5e443fc05edff',
-				'label'             => 'Content 2',
-				'name'              => 'content_2',
-				'type'              => 'wysiwyg',
-				'instructions'      => '',
-				'required'          => 0,
-				'conditional_logic' => 0,
-				'wrapper'           => array(
-					'width' => '',
-					'class' => '',
-					'id'    => '',
-				),
-				'default_value'     => '',
-				'tabs'              => 'all',
-				'toolbar'           => 'full',
-				'media_upload'      => 1,
-				'delay'             => 0,
-			),
-			array(
-				'key'               => 'field_5e44401415548',
-				'label'             => 'Collapse',
-				'name'              => 'collapse',
-				'type'              => 'clone',
-				'instructions'      => '',
-				'required'          => 0,
-				'conditional_logic' => 0,
-				'wrapper'           => array(
-					'width' => '',
-					'class' => '',
-					'id'    => '',
-				),
-				'clone'             => array(
-					0 => 'group_5e16225f53617',
-				),
-				'display'           => 'seamless',
-				'layout'            => 'block',
-				'prefix_label'      => 0,
-				'prefix_name'       => 0,
-			),
-			array(
-				'key'               => 'field_5e44427eb6535',
-				'label'             => 'Content 3',
-				'name'              => 'content_3',
-				'type'              => 'wysiwyg',
-				'instructions'      => '',
-				'required'          => 0,
-				'conditional_logic' => 0,
-				'wrapper'           => array(
-					'width' => '',
-					'class' => '',
-					'id'    => '',
-				),
-				'default_value'     => '',
-				'tabs'              => 'all',
-				'toolbar'           => 'full',
-				'media_upload'      => 1,
-				'delay'             => 0,
-			),
-		),
-		'location'              => array(
-			array(
-				array(
-					'param'    => 'page_template',
-					'operator' => '==',
-					'value'    => 'uns-helfen.php',
-				),
-			),
-		),
-		'menu_order'            => 0,
-		'position'              => 'normal',
-		'style'                 => 'default',
-		'label_placement'       => 'top',
-		'instruction_placement' => 'label',
-		'hide_on_screen'        => '',
-		'active'                => true,
-		'description'           => '',
-	) );
+// TODO: insert fields from lemming's config...
 
-endif;
+//if ( function_exists( 'acf_add_local_field_group' ) ):
+//
+//	acf_add_local_field_group( array(
+//		'key'                   => 'group_5e443fb80dbaa',
+//		'title'                 => 'Uns Helfen Page',
+//		'fields'                => array(
+//			array(
+//				'key'               => 'field_5e443fc05edff',
+//				'label'             => 'Content 2',
+//				'name'              => 'content_2',
+//				'type'              => 'wysiwyg',
+//				'instructions'      => '',
+//				'required'          => 0,
+//				'conditional_logic' => 0,
+//				'wrapper'           => array(
+//					'width' => '',
+//					'class' => '',
+//					'id'    => '',
+//				),
+//				'default_value'     => '',
+//				'tabs'              => 'all',
+//				'toolbar'           => 'full',
+//				'media_upload'      => 1,
+//				'delay'             => 0,
+//			),
+//			array(
+//				'key'               => 'field_5e44401415548',
+//				'label'             => 'Collapse',
+//				'name'              => 'collapse',
+//				'type'              => 'clone',
+//				'instructions'      => '',
+//				'required'          => 0,
+//				'conditional_logic' => 0,
+//				'wrapper'           => array(
+//					'width' => '',
+//					'class' => '',
+//					'id'    => '',
+//				),
+//				'clone'             => array(
+//					0 => 'group_5e16225f53617',
+//				),
+//				'display'           => 'seamless',
+//				'layout'            => 'block',
+//				'prefix_label'      => 0,
+//				'prefix_name'       => 0,
+//			),
+//			array(
+//				'key'               => 'field_5e44427eb6535',
+//				'label'             => 'Content 3',
+//				'name'              => 'content_3',
+//				'type'              => 'wysiwyg',
+//				'instructions'      => '',
+//				'required'          => 0,
+//				'conditional_logic' => 0,
+//				'wrapper'           => array(
+//					'width' => '',
+//					'class' => '',
+//					'id'    => '',
+//				),
+//				'default_value'     => '',
+//				'tabs'              => 'all',
+//				'toolbar'           => 'full',
+//				'media_upload'      => 1,
+//				'delay'             => 0,
+//			),
+//		),
+//		'location'              => array(
+//			array(
+//				array(
+//					'param'    => 'page_template',
+//					'operator' => '==',
+//					'value'    => 'uns-helfen.php',
+//				),
+//			),
+//		),
+//		'menu_order'            => 0,
+//		'position'              => 'normal',
+//		'style'                 => 'default',
+//		'label_placement'       => 'top',
+//		'instruction_placement' => 'label',
+//		'hide_on_screen'        => '',
+//		'active'                => true,
+//		'description'           => '',
+//	) );
+//
+//endif;
--- uns-helfen.php
+++ uns-helfen.php
@@ -3,74 +3,78 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <div class="container mod-uns-helfen">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="post-title"><?php the_title(); ?></h1>
-				<?php the_content(); ?>
+<div class="container mod-helfen">
+
+    <div class="header">
+        <div class="col-12">
+            <h1 class="post-title"><?php the_title(); ?></h1>
+            <div class="wysiwyg">
+                <?php the_content(); ?>
             </div>
         </div>
-        <div class="row below-content">
-            <div class="col-12">
-                <div class="wrapper">
-					<?php the_field( 'content_2' ); ?>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-			<?php
+        <p class="text-intro"><?php the_field('text_intro'); ?></p>
+    </div>
+    <div class="crew-info">
+        <?php $text_bloc = get_field('text_bloc'); ?>
+        <h2><?php echo $text_bloc['group_title']; ?></h2>
+        <p><?php echo $text_bloc['group_text']; ?></p>
+    </div>
+
+
+    <div class="row">
+        <?php
 			global $collaps_id;
 			$collaps_id ++;
 			$id = 'collapse-block-' . $collaps_id;
 			?>
 
-            <div id="<?php echo esc_attr( $id ); ?>" class="mod-collapse col-12">
-                <h2><?php the_sub_field( 'ueberschrift' ); ?></h2>
+        <div id="<?php echo esc_attr( $id ); ?>" class="mod-collapse col-12">
+            <h2><?php the_sub_field( 'ueberschrift' ); ?></h2>
 
-				<?php if ( have_rows( 'fragen' ) ): ?>
+            <?php if ( have_rows( 'fragen' ) ): ?>
 
-                    <div class="accordion" id="<?php echo $id; ?>-item">
-						<?php
+            <div class="accordion" id="<?php echo $id; ?>-item">
+                <?php
 						$count = 1;
 						while ( have_rows( 'fragen' ) ) : the_row();
 							$id_head    = esc_attr( $id . '-item-head-' . $count );
 							$id_content = esc_attr( $id . '-item-content-' . $count );
 							?>
-                            <div class="collapse-item">
-                                <div class="collapse-title" id="<?php echo $id_head; ?>">
-                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#<?php echo $id_content; ?>"
-                                            aria-expanded="true" aria-controls="collapseOne">
-										<?php the_sub_field( 'frage' ); ?>
-                                    </button>
-                                </div>
-                                <div id="<?php echo $id_content; ?>" class="collapse" aria-labelledby="<?php echo $id_head; ?>">
-                                    <div class="card-body">
-										<?php the_sub_field( 'antwort' ); ?>
-                                    </div>
-                                </div>
-                            </div>
-							<?php
+                <div class="collapse-item">
+                    <div class="collapse-title" id="<?php echo $id_head; ?>">
+                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#<?php echo $id_content; ?>" aria-expanded="true" aria-controls="collapseOne">
+                            <?php the_sub_field( 'frage' ); ?>
+                        </button>
+                    </div>
+                    <div id="<?php echo $id_content; ?>" class="collapse" aria-labelledby="<?php echo $id_head; ?>">
+                        <div class="card-body">
+                            <?php the_sub_field( 'antwort' ); ?>
+                        </div>
+                    </div>
+                </div>
+                <?php
 							$count ++;
 						endwhile;
 						?>
-                    </div>
-				<?php
+            </div>
+            <?php
 				else :
 					// no rows found
 
 				endif;
 
 				?>
-            </div>
-        </div>
-        <div class="row below-content">
-            <div class="col-12">
-                <div class="wrapper">
-					<?php the_field( 'content_3' ); ?>
-                </div>
-            </div>
         </div>
     </div>
+    <div class="helper-bloc">
+        <?php $helper_bloc = get_field('inscription_bloc'); ?>
+        <h2><?php echo $helper_bloc['title']; ?></h2>
+        <div class="row">
+            <p><?php echo $helper_bloc['text']; ?></p>
+            <button><?php echo $helper_bloc['button']; ?></button>
+        </div>
+    </div>
+</div>
 <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
