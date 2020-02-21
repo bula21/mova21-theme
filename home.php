84fb1988f7d7973563af74376bf0a80a5ca41f34
Florian Bentele <florian.bentele@alea-iacta.ch>
2020-02-21 16:38:42 +0100

    added home animation and small styling fixes

--- home.php
+++ home.php
@@ -9,7 +9,8 @@
 		// Animation part 1
 		?>
         <div class="row">
-            <div class="home-animation-1">
+            <div class="home-animation-1 col-12">
+                <!--<div class="mod-animation animation" data-animation="home" data-autoplay="true" data-hover="true"></div>-->
                 <img src="<?php echo BULA_URL_TO_THEME; ?>/img/animation-tbd.png">
             </div>
         </div>
@@ -18,9 +19,11 @@
 		// Content Block 1
 		?>
         <div class="row">
-            <div class="col-12 home-content-block-1">
-                <div class="wysiwyg">
-					<?php the_content(); ?>
+            <div class="col-12">
+                <div class="home-content-block-1">
+                    <div class="wysiwyg">
+						<?php the_content(); ?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -36,18 +39,20 @@
                 </div>
             </div>
             <div class="row">
-                <a href="<?php _e( 'Link Pfadis', 'bula21' ); ?>" class="block--item">
-                    <div class="mod-animation animation d-none d-md-block" data-animation="gruppen" data-autoplay="false" data-hover="true"></div>
-                    <div class="bg-black  choice-text"><p class="t-white"><?php _e( 'Pfadis', 'bula21' ); ?></p></div>
-                </a>
-                <a href="<?php _e( 'Link Helfende', 'bula21' ); ?>" class="block--item">
-                    <div class="mod-animation animation d-none d-md-block" data-animation="helfende" data-autoplay="false" data-hover="true"></div>
-                    <div class="bg-yellow choice-text"><p><?php _e( 'Helfende', 'bula21' ); ?></p></div>
-                </a>
-                <a href="<?php _e( 'Link Externe', 'bula21' ); ?>" class="block--item">
-                    <div class="mod-animation animation d-none d-md-block" data-animation="externe" data-autoplay="false" data-hover="true"></div>
-                    <div class="bg-blue choice-text"><p><?php _e( 'Externe', 'bula21' ); ?></p></div>
-                </a>
+                <div class="col-12 d-flex">
+                    <a href="<?php _e( 'Link Pfadis', 'bula21' ); ?>" class="block--item">
+                        <div class="mod-animation animation d-none d-md-block" data-animation="gruppen" data-autoplay="false" data-hover="true"></div>
+                        <div class="bg-black  choice-text"><p class="t-white"><?php _e( 'Pfadis', 'bula21' ); ?></p></div>
+                    </a>
+                    <a href="<?php _e( 'Link Helfende', 'bula21' ); ?>" class="block--item">
+                        <div class="mod-animation animation d-none d-md-block" data-animation="helfende" data-autoplay="false" data-hover="true"></div>
+                        <div class="bg-yellow choice-text"><p><?php _e( 'Helfende', 'bula21' ); ?></p></div>
+                    </a>
+                    <a href="<?php _e( 'Link Externe', 'bula21' ); ?>" class="block--item">
+                        <div class="mod-animation animation d-none d-md-block" data-animation="externe" data-autoplay="false" data-hover="true"></div>
+                        <div class="bg-blue choice-text"><p><?php _e( 'Externe', 'bula21' ); ?></p></div>
+                    </a>
+                </div>
             </div>
         </div>
 
