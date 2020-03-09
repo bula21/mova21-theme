b52c32e125ce2e42582780a36b76ecf920b0d489
Florian Bentele <florian.bentele@alea-iacta.ch>
2020-03-09 19:45:39 +0100

    fixes styling in teilnehmen page

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
  * Global static theme path
  */
 define( 'BULA_URL_TO_THEME', get_stylesheet_directory_uri() );
-define( 'CACHE', WP_DEBUG ? time() : date( 'Ym' ) );
+define( 'CACHE', WP_DEBUG ? time() : date( 'Ym' ) . '1' );
 
 foreach ( glob( __DIR__ . '/acf/*.php' ) as $filename ) {
 	require_once( $filename );
