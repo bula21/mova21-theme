0310eb661d5f148405528be3a121b6b611c60ae4
Lemming <clement.devaulchier@bula21.ch>
2020-05-10 19:53:21 +0200

    groupe page complet redesign

--- groupes.php
+++ groupes.php
@@ -4,64 +4,61 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) :
 	the_post(); ?>
-    <div class="container mod-groupes">
+<div class="container mod-groupes">
     <div class="header">
-       <div class="infos">
-           <h1 class="post-title"><?php the_title(); ?></h1>
-        <p class="text-intro"><?php the_field( 'intro_text' ); ?></p>
+        <div class="infos">
+            <h1 class="post-title"><?php the_title(); ?></h1>
+            <p class="text-intro"><?php the_field( 'intro_text' ); ?></p>
         </div>
         <div class="illustration">
             <img src="" alt="">
         </div>
     </div>
-    <div class="timeLine">
-        <h2 class="tl-title"></h2>
-		<?php
+    <div class="body-container">
+        <div class="timeLine">
+            <?php
 		if ( have_rows( 'group_timeline' ) ):
 			while ( have_rows( 'group_timeline' ) ) : the_row();
 				if ( have_rows( 'timeline_event' ) ):
 					while ( have_rows( 'timeline_event' ) ) : the_row(); ?>
-                        <div class="event">
-                            <svg class="arrow" viewBox="0 -2 24 24">
-                                <path d="M24 22h-24l12-4z"/>
-                            </svg>
-                            <div class="cta-bloc">
-                                <p class="date"><?php the_sub_field( 'event_date' ); ?></p>
-                                <p class="eventName"><?php the_sub_field( 'event_name' ); ?></p>
-								<?php if ( get_sub_field( 'event_button' ) ): $link = get_sub_field( 'event_button' ); ?>
-                                    <a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>">
-                                        <p class="cta-button"><?php echo $link['title']; ?> →</p>
-                                    </a>
-								<?php endif; ?>
-                            </div>
-                        </div>
-					<?php endwhile;
+            <div class="tl-event">
+                <div class="cta-bloc">
+                    <p class="date"><?php the_sub_field( 'event_date' ); ?></p>
+                    <p class="eventName"><?php the_sub_field( 'event_name' ); ?></p>
+                    <?php if ( get_sub_field( 'event_button' ) ): $link = get_sub_field( 'event_button' ); ?>
+                    <a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>">
+                        <p class="cta-button"><?php echo $link['title']; ?> →</p>
+                    </a>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <?php endwhile;
 				else : endif;
 			endwhile;
 		else : endif;
 		?>
-    </div>
-    <div class="event-infos">
-		<?php
-
+        </div>
+        <div class="events-container">
+            <?php
 		if ( have_rows( 'tm-event-info' ) ):
 			while ( have_rows( 'tm-event-info' ) ) : the_row(); ?>
-                <div class="info">
-                    <p class="date"><?php the_sub_field( 'event_date' ); ?></p>
+            <div class="event">
+                <div class="title">
                     <h2><?php the_sub_field( 'event_name' ); ?></h2>
-                    <div class="callToAction">
-                        <p class="text"><?php the_sub_field( 'event_text' ); ?></p>
-						<?php if ( get_sub_field( 'event_button' ) ): $link = get_sub_field( 'event_button' ); ?>
-                            <a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>">
-                                <button class="btn-black"><?php echo $link['title']; ?></button>
-                            </a>
-						<?php endif; ?>
-                    </div>
+                    <?php if ( get_sub_field( 'event_button' ) ): $link = get_sub_field( 'event_button' ); ?>
+                    <a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>">
+                        <button class="btn-black"><?php echo $link['title']; ?></button>
+                    </a>
+                    <?php endif; ?>
                 </div>
-			<?php endwhile;
+                <p class="text"><?php the_sub_field( 'event_text' ); ?></p>
+            </div>
+            <?php endwhile;
 		else : endif;
 		?>
+        </div>
     </div>
+</div>
 <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
