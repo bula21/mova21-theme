7c61d0c9b69bd1f71d0995f794248f66dfb2ed62
Florian Bentele <florian.bentele@alea-iacta.ch>
2021-07-12 22:51:46 +0200

    adds countdown element

--- home.php
+++ home.php
@@ -65,7 +65,49 @@
                 </div>
             </div>
         </div>
+		<?php if ( $datetime = get_field( 'countdowndate', 'options' ) ): ?>
+            <div class="home-content-block-countdown" data-time="<?php echo wp_date( 'U', $datetime ); ?>">
+                <div class="row">
+                    <div class="col">
+                        <h2><?php _e( 'bis ins mova', 'bula21' ); ?></h2>
+                    </div>
+                </div>
+                <div class="row container">
+                    <div class="col-12 col-md-3 bg-blue">
+                        <div class="number days">
 
+                        </div>
+                        <div class="time-entity ">
+							<?php _e( 'Tage', 'bula21' ); ?>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-3 bg-red">
+                        <div class="number hours">
+
+                        </div>
+                        <div class="time-entity">
+							<?php _e( 'Stunden', 'bula21' ); ?>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-3 bg-yellow">
+                        <div class="number minutes">
+
+                        </div>
+                        <div class="time-entity">
+							<?php _e( 'Minuten', 'bula21' ); ?>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-3 bg-black font-yellow">
+                        <div class="number seconds">
+
+                        </div>
+                        <div class="time-entity">
+							<?php _e( 'Sekunden', 'bula21' ); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+		<?php endif; ?>
 		<?php
 		// Content Block 3 - News
 		$query = new WP_Query(
