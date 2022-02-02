7f6add93a090f03750848884d750cb370b173584
Florian Bentele <florian.bentele@bula21.ch>
2022-02-02 14:32:49 +0100

    adds styling for teilnehmen template

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
  * Global static theme path
  */
 define( 'BULA_URL_TO_THEME', get_stylesheet_directory_uri() );
-define( 'CACHE', WP_DEBUG ? time() : '20210119' );
+define( 'CACHE', WP_DEBUG ? time() : '20210202' );
 
 foreach ( glob( __DIR__ . '/acf/*.php' ) as $filename ) {
 	require_once( $filename );
