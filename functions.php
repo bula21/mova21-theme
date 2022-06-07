82b0ee7c0f293a328e500f0a7f1179b85b80791a
Florian Bentele <florian.bentele@bula21.ch>
2022-06-07 20:09:51 +0200

    fixes rest markup content

--- functions.php
+++ functions.php
@@ -285,3 +285,53 @@ function bula_count_submissions_with_date( $date ) {
 
 	return $has_date;
 }
+
+
+add_filter( 'rest_prepare_post', function ( $post_response, $post, $context ) {
+	ob_start();
+	$args = array( 'ID' => $post->ID );
+	query_posts( $args );
+	get_template_part( 'single' );
+	$full_content = ob_get_clean();
+	wp_reset_postdata();
+	$post_response->data['content_api'] = trim( $full_content );
+
+	return $post_response;
+
+}, 0, 3 );
+
+
+if ( ! function_exists( 'is_rest' ) ) {
+	/**
+	 * Checks if the current request is a WP REST API request.
+	 *
+	 * Case #1: After WP_REST_Request initialisation
+	 * Case #2: Support "plain" permalink settings and check if `rest_route` starts with `/`
+	 * Case #3: It can happen that WP_Rewrite is not yet initialized,
+	 *          so do this (wp-settings.php)
+	 * Case #4: URL Path begins with wp-json/ (your REST prefix)
+	 *          Also supports WP installations in subfolders
+	 *
+	 * @returns boolean
+	 * @author matzeeable
+	 */
+	function is_rest() {
+		if ( defined( 'REST_REQUEST' ) && REST_REQUEST // (#1)
+		     || isset( $_GET['rest_route'] ) // (#2)
+		        && strpos( $_GET['rest_route'], '/', 0 ) === 0 ) {
+			return true;
+		}
+
+		// (#3)
+		global $wp_rewrite;
+		if ( $wp_rewrite === null ) {
+			$wp_rewrite = new WP_Rewrite();
+		}
+
+		// (#4)
+		$rest_url    = wp_parse_url( trailingslashit( rest_url() ) );
+		$current_url = wp_parse_url( add_query_arg( array() ) );
+
+		return strpos( $current_url['path'] ?? '/', $rest_url['path'], 0 ) === 0;
+	}
+}
