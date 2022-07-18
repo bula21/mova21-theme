7002258ec49accfc0c64e438a729e1d7088f3343
Florian Bentele <florian.bentele@bula21.ch>
2022-07-18 14:26:33 +0200

    fixes 2col image

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
  * Global static theme path
  */
 define( 'BULA_URL_TO_THEME', get_stylesheet_directory_uri() );
-define( 'CACHE', WP_DEBUG ? time() : '202207175' );
+define( 'CACHE', WP_DEBUG ? time() : '202207176' );
 
 foreach ( glob( __DIR__ . '/acf/*.php' ) as $filename ) {
 	require_once( $filename );
