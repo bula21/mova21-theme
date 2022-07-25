b127bbf56379161b120d9d0d677e0f172b8da57c
Florian Bentele <florian@kdgs.ch>
2022-07-25 14:18:27 +0200

    gimme some more space

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
  * Global static theme path
  */
 define( 'BULA_URL_TO_THEME', get_stylesheet_directory_uri() );
-define( 'CACHE', WP_DEBUG ? time() : '202207177' );
+define( 'CACHE', WP_DEBUG ? time() : '202207178' );
 
 foreach ( glob( __DIR__ . '/acf/*.php' ) as $filename ) {
 	require_once( $filename );
